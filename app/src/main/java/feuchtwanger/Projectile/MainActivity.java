40cdba946497045822dd67578fd84f8925e01093
unknown <student1@FB-W213-W8.academic.touro.edu>
2015-10-15 21:23:43 -0400

    Stuff happened.

--- app/src/main/java/feuchtwanger/Projectile/MainActivity.java
+++ app/src/main/java/feuchtwanger/Projectile/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
         angle = (TextView) findViewById(R.id.angle);
         angleTB = (EditText) findViewById(R.id.angleTB);
         velocity = (TextView) findViewById(R.id.velocity);
